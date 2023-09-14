5ba5e809349dd3f92dab1eb118eae3fb927f429e
= <hiradcodes@yahoo.com>
2023-09-14 18:03:16 +0300

    checkoint1

--- include/segment.h
+++ include/segment.h
@@ -1,13 +1,19 @@
 #ifndef _SEGMENT_H_
 #define _SEGMENT_H_
 
-namespace app{
-	class SegmentData{
-		int image_width;
-		int image_height;
-		int target_width;
-		int target_height;
-	};
-}
+class SegmentData{
+	int image_width;
+	int image_height;
+	int target_width;
+	int target_height;
+	// TODO: define more private fields here
+	
+	// TODO: declare methods here
+	public:
+	SegmentData(int32_t image_width,
+			    int32_t image_height,
+				int out_width,
+				int out_height);
+};
 
 #endif
---
+++ src/common/segment.cpp
@@ -0,0 +1,11 @@
+#include "segment.h"
+#include "common.h"
+
+SegmentData::SegmentData(int32_t image_width,
+			    int32_t image_height,
+				int out_width,
+				int out_height){
+	//TODO: define constructor here
+	//perform segmentation and provide
+	//results in some public funcs.
+}
